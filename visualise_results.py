18941d5173cee14121064586e7fb08b8c691581a
Samuel Leong <leowhiz@yahoo.com.sg>
2020-01-29 18:49:32 -0500

    Moved visualise outside to use mvn utils

--- visualise_results.py
+++ visualise_results.py
@@ -1,9 +1,11 @@
 from mvn.utils.vis import *
 
-results_file = "results.pkl"
+results_file = "results/results.pkl"
 
 with open(fileName, "rb") as f:
     data = pickle.load(f)
     keypoints3d = data["keypoints_3d"]
     indexes = data["indexes"]
 
+draw_3d_pose(keypoints3d, axes3d)
+plt.show()

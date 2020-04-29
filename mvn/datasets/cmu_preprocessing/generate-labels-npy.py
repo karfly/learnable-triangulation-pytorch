861a24a36cc59b9063711b75cc421191b4bd75ed
Samuel Leong <leowhiz@yahoo.com.sg>
2020-04-30 00:17:47 +0800

    Debug test

--- mvn/datasets/cmu_preprocessing/generate-labels-npy.py
+++ mvn/datasets/cmu_preprocessing/generate-labels-npy.py
@@ -318,7 +318,7 @@ def load_table_segment(data, action_idx, action_name):
             try:
                 bbox[camera_idx] = bbox_data[action_name][camera_name][int(frame_nm)]
             except:
-                raise f"bbox[{camera_idx}] failed: {action_name} {camera_name} {frame_nm}")
+                raise f"bbox[{camera_idx}] failed: {action_name} {camera_name} {frame_nm}"
 
     for frame_idx, frame_name in enumerate(data['valid_frames']):
         # TODO: Poses changing from CMU to H36M, if the current one doesn't do it automatically

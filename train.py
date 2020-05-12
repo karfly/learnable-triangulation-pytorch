74bcb6c601316d8fc660b11a0c8638bb04c79f0a
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-12 23:11:33 +0800

    More trace

--- train.py
+++ train.py
@@ -248,16 +248,12 @@ def one_epoch(model, criterion, opt, config, dataloader, device, epoch, n_iters_
         end = time.time()
 
         iterator = enumerate(dataloader)
-        print(len(dataloader))
+        print(len(dataloader)); print("")
 
         if is_train and config.opt.n_iters_per_epoch is not None:
             iterator = islice(iterator, config.opt.n_iters_per_epoch)
 
-        ipdb.set_trace()
-
         for iter_i, batch in iterator:
-            ipdb.set_trace()
-
             print(iter_i)
 
             with autograd.detect_anomaly():

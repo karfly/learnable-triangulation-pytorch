3a7fc9a5700dbe3d23c74d377c2b92415e53c628
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-13 22:25:22 +0800

    what 2

--- mvn/datasets/utils.py
+++ mvn/datasets/utils.py
@@ -54,7 +54,7 @@ def prepare_batch(batch, device, config, is_train=True):
 
     # 3D keypoints
     # TODO: Why cut off only the confidences here?
-    keypoints_3d_batch_gt = torch.from_numpy(np.stack(batch['keypoints_3d'], axis=0)[:, :, :]).float().to(device)
+    keypoints_3d_batch_gt = torch.from_numpy(np.stack(batch['keypoints_3d'], axis=0)[:, :, 3:]).float().to(device)
 
     # 3D keypoints validity
     keypoints_3d_validity_batch_gt = torch.from_numpy(np.stack(batch['keypoints_3d'], axis=0)[:, :, 3:]).float().to(device)

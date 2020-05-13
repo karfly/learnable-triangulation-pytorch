4fdc0ddbd58d1d99fc3e5d9e0b5beffc872148cc
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-13 22:57:44 +0800

    Force coco vis

--- train.py
+++ train.py
@@ -373,10 +373,10 @@ def one_epoch(model, criterion, opt, config, dataloader, device, epoch, n_iters_
                 # TODO: see if transfer_cmu_h36m visualisation error
                 if master:
                     if n_iters_total % config.vis_freq == 0:# or total_l2.item() > 500.0:
-                        vis_kind = config.kind
+                        vis_kind = config.kind if config.kind != "cmu" else "coco"
                         if (config.transfer_cmu_to_human36m if hasattr(config, "transfer_cmu_to_human36m") else False):
                             vis_kind = "coco"
-
+                        
                         for batch_i in range(min(batch_size, config.vis_n_elements)):
                             # TODO: Plot images without tensorboard?
                             

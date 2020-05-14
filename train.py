a2705e6f6f612a7becc6e101d5d80c102d7af7ca
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-14 22:00:23 +0800

    debugging 2

--- train.py
+++ train.py
@@ -282,7 +282,7 @@ def one_epoch(model, criterion, opt, config, dataloader, device, epoch, n_iters_
                     keypoints_3d_pred, heatmaps_pred, volumes_pred, confidences_pred, cuboids_pred, coord_volumes_pred, base_points_pred = model(images_batch, proj_matricies_batch, batch)
 
                 # TODO: What is batch shape[2]; check batch shape
-                print(images_batch)
+                print(images_batch.shape)
 
                 batch_size, n_views, image_shape = images_batch.shape[0], images_batch.shape[1], tuple(images_batch.shape[3:])
                 n_joints = keypoints_3d_pred[0].shape[1]

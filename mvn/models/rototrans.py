90b433c9800a92dd8d0d60aee347ddd067b0be9d
stefano <sirfoga@protonmail.com>
2021-05-21 19:17:59 +0200

    256 x622

--- mvn/models/rototrans.py
+++ mvn/models/rototrans.py
@@ -14,15 +14,10 @@ class RototransNet(nn.Module):
 
         n_features = config.cam2cam.model.n_features
 
-        f0 = 512
-        f1 = 256
-        f2 = 128
-        f3 = 64
-
         self.backbone = nn.Sequential(*[
             nn.Flatten(),  # will be fed into a MLP
             MLPResNet(
-                2 * n_joints * 2, config.cam2cam.model.backbone.inner_size, 2, f0,
+                2 * n_joints * 2, config.cam2cam.model.backbone.inner_size, 2, n_features,
                 batch_norm=batch_norm,
                 drop_out=drop_out,
                 activation=nn.LeakyReLU,
@@ -31,28 +26,7 @@ class RototransNet(nn.Module):
 
         self.R_backbone = nn.Sequential(*[
             MLPResNet(
-                f0, f0, config.cam2cam.model.roto.n_layers,
-                f1,
-                batch_norm=batch_norm,
-                drop_out=drop_out,
-                activation=nn.LeakyReLU,
-            ),
-            MLPResNet(
-                f1, f1, config.cam2cam.model.roto.n_layers,
-                f2,
-                batch_norm=batch_norm,
-                drop_out=drop_out,
-                activation=nn.LeakyReLU,
-            ),
-            MLPResNet(
-                f2, f2, config.cam2cam.model.roto.n_layers,
-                f3,
-                batch_norm=batch_norm,
-                drop_out=drop_out,
-                activation=nn.LeakyReLU,
-            ),
-            MLPResNet(
-                f3, f3, config.cam2cam.model.roto.n_layers,
+                n_features, n_features, config.cam2cam.model.roto.n_layers,
                 6 if config.cam2cam.model.roto.parametrization == '6d' else 3,
                 batch_norm=batch_norm,
                 drop_out=drop_out,
@@ -63,28 +37,7 @@ class RototransNet(nn.Module):
 
         self.t_backbone = nn.Sequential(*[
             MLPResNet(
-                f0, f0, config.cam2cam.model.trans.n_layers,
-                f1,
-                batch_norm=batch_norm,
-                drop_out=drop_out,
-                activation=nn.LeakyReLU,
-            ),
-            MLPResNet(
-                f1, f1, config.cam2cam.model.trans.n_layers,
-                f2,
-                batch_norm=batch_norm,
-                drop_out=drop_out,
-                activation=nn.LeakyReLU,
-            ),
-            MLPResNet(
-                f2, f2, config.cam2cam.model.trans.n_layers,
-                f3,
-                batch_norm=batch_norm,
-                drop_out=drop_out,
-                activation=nn.LeakyReLU,
-            ),
-            MLPResNet(
-                f3, f3, config.cam2cam.model.trans.n_layers,
+                n_features, n_features, config.cam2cam.model.trans.n_layers,
                 3,
                 batch_norm=batch_norm,
                 drop_out=drop_out,
